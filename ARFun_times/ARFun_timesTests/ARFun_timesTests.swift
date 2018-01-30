2ccaa0ed6b620a95b2d0dfd01ee0c00ef162fd49
jma <jma@trifork.com>
2018-01-30 16:51:35 +0100

    project setup

---
+++ ARFun_times/ARFun_timesTests/ARFun_timesTests.swift
@@ -0,0 +1,36 @@
+//
+//  ARFun_timesTests.swift
+//  ARFun_timesTests
+//
+//  Created by Jeppe Malmberg on 30/01/2018.
+//  Copyright © 2018 Malmbergjeppe. All rights reserved.
+//
+
+import XCTest
+@testable import ARFun_times
+
+class ARFun_timesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
